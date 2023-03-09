032bfbde9602e9b13f806090a256c6fd58c2074c
Niklas Simonsen <nsimonsen@grundfos.com>
2023-03-09 20:36:26 +0100

    Final scatter script

--- script.js
+++ script.js
@@ -5,10 +5,10 @@ const url = 'https://raw.githubusercontent.com/FreeCodeCamp/ProjectReferenceData
 
 function scatterPlot(data) {
 
-    const margin = {top: 50, right: 50, left: 50, bottom: 120},
+    const margin = {top: 50, right: 50, left: 70, bottom: 50},
     width = 800 - margin.left - margin.right,
-    height = 600 - margin.top - margin.bottom;
-
+    height = 500 - margin.top - margin.bottom,
+    padding_top = 50;
     const xArrYears = data.map((d) => {
         return new Date("" + d.Year + "-01-01")
     });
@@ -54,36 +54,36 @@ function scatterPlot(data) {
 
     let toolTip = d3.select('.container')
         .append("div")
-            .style("position","absolute")
-            .attr("id","tooltip")
-            .style("background","white")
-            .style("display","none")
-            .style("border-radius","5px")
-            .style("padding","5px")
-            .classed('tooltip',true)
+        .classed('tooltip',true)
+        .style("position","absolute")
+        .style("background","lightblue")
+        .style("border-radius","5px")
+        .style("width","200px")
+        .style("padding","5px")
+        .style("display","none")
+        .attr("id","tooltip")
+
     toolTip.append('div')
         .classed("person_nationality", true)
-        .classed("age_time",true)
-        .classed("drugged",true)
 
     // Add an svg object to the body of the page
     let svg = d3.select('.container')
         .append('svg')
         .attr('width', width + margin.left + margin.right)
-        .attr('height', height + margin.top + margin.bottom)
+        .attr('height', height + margin.top + margin.bottom + padding_top)
         .append('g')
         .attr("transform", "translate(" + margin.left + "," + margin.right + ")");
 
     // Add x-axis
     svg.append("g")
-    .attr("transform", "translate(0, " + (height) +")")
+    .attr("transform", "translate(0, " + (height + padding_top) +")")
     .classed('axis', true)
     .attr('id','x-axis')
     .call(xAxis)
 
     // Add y-axis
     svg.append("g")
-    .attr("transform", "translate(" + (0) +", 0)")
+    .attr("transform", "translate(" + (0) +", "+ (padding_top) +" )")
     .classed('axis', true)
     .attr('id','y-axis')
     .call(yAxis);
@@ -92,32 +92,98 @@ function scatterPlot(data) {
     svg.selectAll('circle')
     .data(data)
     .enter().append('circle')
-        .attr('class','dot')
-        .attr('cx', d => xScale(d.Year))
-        .attr('cy', d => yScale(d.Seconds))
-        .attr('r', 8.5)
-        .style("fill", (d) => color(d.bDoped))
+    .style("fill", (d) => color(d.bDoped))
+    .classed("dot",true)
+    .attr('cx', 0)
+    .attr('cy', 0)
+    .attr('r', 8.5)
+    // Add hover effect
     .on('mouseover', function(d) {
         let data_Mouse = d.target.__data__;
+        //Save temporary color
+        tempColor = this.style.fill
         toolTip.transition()
             .style('display','block')
-            .style('opacity',1)
-            
+            .style('opacity',0.9)
+            .style('left', (d.pageX+20)+'px')
+            .style('top', (d.pageY-80)+'px')
+
         d3.select('.tooltip .person_nationality')
             .html(() => 'Name: ' + data_Mouse.Name + ', Nationality: ' + data_Mouse.Nationality +
             '<br>' + 'Year: ' + data_Mouse.Year + ' Time: ' + data_Mouse.Time + '<br><br>' +
             'Doping: ' + data_Mouse.Doping)
-            .style('background','white')
-            .style('font-size', '1.2em')
+            .style('background','lightblue')
+            .style('font-size', '0.8em')
             .style('font-weight', 'bold')
-        toolTip
-            .style('left', (d.pageX-145)+'px')
-            .style('top', (d.pageY-45)+'px')
+            
         d3.select(this)
             .style('opacity', 0.5)
             .attr('r', 10)
+        
         console.log(data_Mouse)
     })
+    // Add hover off effect
+    .on('mouseout', function(d) {
+        // Remove tooltip
+        toolTip.transition()
+        .style('display','none')
+        // Change color of rect back to normal
+        d3.select(this)
+        .style('opacity',1)
+        .attr('r', 8.5)
+        // .style('fill', tempColor)
+    })
+    .transition()
+        .attr('cx', d => xScale(d.Year))
+        .attr('cy', d => yScale(d.Seconds)+padding_top)
+        .delay((d, i) => i * 5)
+        .duration(1000)
+        .ease(d3.easeElastic)
+
+    d3.select('svg')
+        .append('text')
+          .classed('ylabelText', true)
+          .text('Time in Minutes')
+          .attr('transform', 'translate(' + (margin.left - 50) + ', ' + (margin.top+padding_top) + ') rotate(-90)')
+          .style('text-anchor', 'end')
+    
+    d3.select('svg')
+        .append('text')
+        .classed('titleText', true)
+        .text('Doping in Professional Bicycle Racing')
+        .attr('transform', 'translate(' + ((width+ margin.right + margin.left)/2) + ', ' + (margin.top-20) + ')')
+        .style('text-anchor', 'middle')
+    d3.select('svg')
+        .append('text')
+        .classed('supTitleText', true)
+        .text("35 Fastest times up Alpe d'Huez")
+        .attr('transform', 'translate(' + ((width+ margin.right + margin.left)/2) + ', ' + (margin.top+20) + ')')
+        .style('text-anchor', 'middle')
+    console.log(d3.max(yArrRunTimeNum))
+    svg.append("circle")
+        .attr("cx", (d) => xScale(maxYear))
+        .attr("cy", (d) => yScale(2300))
+        .attr("r", 10)
+        .attr("fill", color(-1));
+    svg.append("text")
+        .attr("x", (d) => xScale(maxYear-0.5))
+        .attr("y", (d) => yScale(2302.2))
+        .attr("text-anchor", "left")
+        .attr("class", "legend")
+        .text("No doping allegations")
+        .style('text-anchor', 'end')
+    svg.append("circle")
+        .attr("cx", (d) => xScale(maxYear))
+        .attr("cy", (d) => yScale(2320))
+        .attr("r", 10)
+        .attr("fill", color(1));
+    svg.append("text")
+        .attr("x", (d) => xScale(maxYear-0.5))
+        .attr("y", (d) => yScale(2322.2))
+        .attr("text-anchor", "left")
+        .attr("class", "legend")
+        .text("Riders with doping allegations")
+        .style('text-anchor', 'end')
 }
 
 async function scatterPlotAsync(dataURL) {
